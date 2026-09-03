9c4ced5c0cf4c767e58f03c3a83f6e33b06936e8
Mayank <mayanksonwani078@gmail.com>
2026-09-04 01:30:18 +0530

    fix(build): add missing Spring Service and Async imports to EmailServiceImpl

--- taaskr-backend/src/main/java/com/taaskr/service/impl/EmailServiceImpl.java
+++ taaskr-backend/src/main/java/com/taaskr/service/impl/EmailServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 
 import java.util.Properties;
 
